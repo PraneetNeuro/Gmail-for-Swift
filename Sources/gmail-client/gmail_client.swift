1045ec61f73a2f87191fb40ea5832918b63910a8
Praneet <praneetquantumx@gmail.com>
2021-04-08 20:57:09 +0530

    Add: API for stop receiving push notifications

--- Sources/gmail-client/gmail_client.swift
+++ Sources/gmail-client/gmail_client.swift
@@ -9,8 +9,14 @@ class Gmail {
     }
     
     class Users {
+        
         static func getProfile(userID: String) -> Data? {
             return API.executeRequest(APIRequest: API.user.getProfile(userID: userID).request, headers: ["Authorization" : "Bearer "], requestBody: nil)
         }
+        
+        static func stop(userID: String) -> Data? {
+            return API.executeRequest(APIRequest: API.user.stop(userID: userID).request, headers: ["Authorization" : "Bearer "], requestBody: nil)
+        }
+        
     }
 }
