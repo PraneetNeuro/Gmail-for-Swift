e4579d28b45493d8d5902863fb56f6fb886d2161
Praneet <praneetquantumx@gmail.com>
2021-04-12 15:04:41 +0530

    Folder structutre changed

--- Package.swift
+++ Package.swift
@@ -24,9 +24,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Smail",
-            dependencies: []),
-        .testTarget(
-            name: "Smail-clientTests",
-            dependencies: ["Smail-client"]),
+            dependencies: [])
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import gmail_clientTests
-
-var tests = [XCTestCaseEntry]()
-tests += gmail_clientTests.allTests()
-XCTMain(tests)
